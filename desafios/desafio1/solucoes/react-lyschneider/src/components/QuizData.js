65eff35bf458eed374d2af9f1101068f3a8e5eb1
Lilian <lilian.schneider@gmail.com>
2020-10-27 01:35:23 -0300

    first commit
    first commit

---
+++ desafios/desafio1/solucoes/react-lyschneider/src/components/QuizData.js
@@ -0,0 +1,32 @@
+export const QuizData = [
+    {
+        id: 0,
+        question: `What is the capital of Brazil?`,
+        options: [`New Delhi`, `Brasilia`, `Owerri`, `Enugu`, `São Paulo`],
+        answer: `Brasilia`
+    },
+    {
+        id: 0,
+        question: `What is the capital of USA?`,
+        options: [`New Delhi`, `Abuja`, `Mumbai`, `Aba`, `Washington DC`],
+        answer: `Washington DC`
+    },
+    {
+        id: 0,
+        question: `What is the capital of Australia?`,
+        options: [`Melbourne`, `Akokwa`, `Owerri`, `Sydney`, `Omaha`],
+        answer: `Sydney`
+    },
+    {
+        id: 0,
+        question: `What is the capital of Turkey?`,
+        options: [`Rijadh`, `Ankara`, `Istanbul`, `Abakaliki`, `Omaha`],
+        answer: `Ankara`
+    },
+    {
+        id: 0,
+        question: `What is the capital of Syria?`,
+        options: [`Syria`, `Damascus`, `Anambra`, `Enugu`, `Omaha`],
+        answer: `Damascus`
+    },
+]
---
+++ desafios/desafio1/solucoes/react-lyschneider/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Quiz from './components/Quiz';
+
+import './styles.css';
+
+function App() {
+    return (
+        <div className="App">
+              <Quiz />
+        </div>
+    )
+}
+
+export default App 
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App></App>, rootElement) //renders the component(first parameter) as a child of the element(second parameter)
+
