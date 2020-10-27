68e090f1b87f6a570e026aadfbafd3ca4d281814
Lilian <lilian.schneider@gmail.com>
2020-10-27 18:02:45 -0300

    next commit

---
+++ desafios/desafio1/solucoes/react-lyschneider/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Quiz from './components/Quiz';
+
+import './styles.css';
+
+function App() {
+    return (
+        <div className="App">
+              <Quiz />
+        </div>
+    )
+}
+
+export default App 
--- desafios/desafio1/solucoes/react-lyschneider/src/index.js
+++ desafios/desafio1/solucoes/react-lyschneider/src/index.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Quiz from './components/Quiz';
+import App from './App';
 
-import './styles.css';
-
-function App() {
-    return (
-        <div className="App">
-              <Quiz />
-        </div>
-    )
-}
-
-export default App 
-
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App></App>, rootElement) //renders the component(first parameter) as a child of the element(second parameter)
-
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
